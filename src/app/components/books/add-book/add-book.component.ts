03074ce3ff95ea2212a9b66099d3942b3dfe2196
makie125 <mwm4923@uncw.edu>
2020-04-15 01:25:10 -0400

    add-book component

--- src/app/components/books/add-book/add-book.component.ts
+++ src/app/components/books/add-book/add-book.component.ts
@@ -1,26 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { LibraryService } from 'src/app/services/library.service';
 
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
-  styleUrls: ['./add-book.component.css']
+  styleUrls: ['../../../../styles.css']
 })
 export class AddBookComponent implements OnInit {
+  @ViewChild('ISBN', { static: true }) ISBN: ElementRef;
+  @ViewChild('author', { static: true }) author: ElementRef;
+  @ViewChild('title', { static: true }) title: ElementRef;
+  @ViewChild('year', { static: true }) year: ElementRef;
+  @ViewChild('pages', { static: true }) pages: ElementRef;
+  @ViewChild('numOfCopies', { static: true }) numOfCopies: ElementRef;
+
   data = {
-    b_id: null,
-    m_id: null,
-    l_id: null
+    ISBN: null,
+    author: null,
+    title: null,
+    year: null,
+    pages: null,
+    numOfCopies: null
   }
   constructor(private libraryService: LibraryService){}
   ngOnInit() {
   }
 
   addBook() {
-    this.libraryService.checkoutBook(this.data)
+    this.libraryService.addBook(this.data)
       .then((response) => {
         this.clearInputs();
       });
   }
-
+  clearInputs() {
+    this.ISBN.nativeElement.value ='';
+    this.author.nativeElement.value ='';
+    this.title.nativeElement.value ='';
+    this.year.nativeElement.value ='';
+    this.pages.nativeElement.value ='';
+    this.numOfCopies.nativeElement.value ='';
+  }
 }
