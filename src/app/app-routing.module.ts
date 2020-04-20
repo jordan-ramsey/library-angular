c109b3cb245957eb365fe331fcac4acf71eed917
JordanRamsey <jrams1414@gmail.com>
2020-04-19 23:09:00 -0400

    fix name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
-<<<<<<< HEAD
-import { ReturnBookComponent } from './components/books/return-book/return-book.component';
-=======
 import { AddMemberComponent } from './components/members/add-member/add-member.component';
->>>>>>> a6aac550fd4b5894ef95f421a413c5a4afb9c081
 
 
 const routes: Routes = [
@@ -26,7 +22,7 @@ const routes: Routes = [
       { path: 'checkout', component: CheckoutBookComponent },
       {path: 'addMember', component: AddMemberComponent},
       { path: 'add', component: AddBookComponent },
-      {path: 'return', component: ReturnBookComponent}
+      {path: 'return', component: ReturnComponent}
     ]
   }
 ];
